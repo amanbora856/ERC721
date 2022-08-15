8c4ba1198273e84e2be20c9e3e439ecaf532dd90
amanbora856 <amanbora856>
2022-08-16 01:47:09 +0530

    added staking nft id-2

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+//0x1cCEA167e6e3734e09635246C8aD72580dd6c36F    contract address where my nft contract is deployed
